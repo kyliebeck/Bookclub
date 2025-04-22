d424a01f352fc1f78c2656a679d1cba10202ce5b
KylieBeck <kylie.beck12@gmail.com>
2025-04-21 17:31:27 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const session = require('express-session');
+// require our new middleware!
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+const authController = require('./controllers/auth.js');
+
+const app = express();
+const port = process.env.PORT ? process.env.PORT : '3000';
+const path = require('path');
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
+// apply our css rules.
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+
+app.use(passUserToView); // use new passUserToView middleware here
+
+app.listen(port, () => {
+    console.log(`The express app is ready on port ${port}!`);
+});
